20565ac9d5ab65f5fb9707fa02884053f40190c5
jawhaa1224 <jawkhlan626@gmail.com>
2022-08-31 00:45:35 +0800

    neg aldaa zassan

--- JS_2/check.js
+++ JS_2/check.js
@@ -34,10 +34,12 @@ const users = {
 //includes ni Array dotorh bvh utguudiig negen zereg baigaa uguigiin shalgaad baival true utgiig baihgui bol false utgiig ilgeedeg.Includes ni negen zereg bvgdiin shalgadag bolohoor && ashiglahguigeer || ashiglaval 1 ni l zov baihad true utgiig ilgeene.Gehdee heregtei ashiglana || -iig.
 const array = Object.keys(users);
 const includes = array.includes("Alex" && "Brook" && "Asab");
-console.log(includes)
+console.log(includes);
 
 //Filter ni Includees yalgaatain Include bvgdiin negen zereg shalgadag bol Filter 1 1-eer ni shalgaad Baih yum bol ter utgiin oor neg huvisagchruu Array bolgon oruulj ogdog filter deer && ashiglaj shalgaj bolohgui tegeh yum bol hoid taliih ni hed ni Arrayruu nemegdej ogohgui || uyed ali negen bolj bgaa bolohoor tus tusdaa nemegdej bolj bgaa.
-const filtered = array.filter((a) => a === "Alex" || "Asab" || "Brook");
+const filtered = array.filter(
+  (a) => a === "Alex" || a === "Asab" || a === "Brook"
+);
 console.log(filtered.length);
 console.log(filtered);
 console.log(filtered.length === 3 ? "manai hed" : "Tanai hed");
@@ -46,4 +48,3 @@ const filtereed = array.filter((a) => a === "Alex" && "Asab" && "Brook");
 console.log(filtereed);
 console.log(filtereed.length);
 console.log(filtereed.length === 3 ? "manai hed" : "Tanai hed");
-
