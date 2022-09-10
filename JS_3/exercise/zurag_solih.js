ed3527d43bcff65111e42d072233aeba68f6fced
jawhaa1224 <jawkhlan626@gmail.com>
2022-09-10 23:07:41 +0800

    toonii mashin

---
+++ JS_3/exercise/zurag_solih.js
@@ -0,0 +1,16 @@
+// let test = document.getElementsByTagName("button")[0];
+
+// test.style ="padding:15px 25px;border-radius:6px;background-color:teal;color:white;border:0;width:100px;";
+// document.getElementsByTagName("img")[0].style = "width:300px;height:300px";
+let source = document.getElementsByTagName[0]("img").src;
+let happy = "happy.png";
+let embrass = "embrass.png";
+
+// const myFunction = () => {
+// if(image == "happy.png"){
+//     image = "embrass.png";
+// }else{
+//     image = "happy.png";
+// }
+// }
+console.log(source);
--- JS_6/calculator.js
+++ JS_6/calculator.js
@@ -1,24 +1,53 @@
 let niittoo = document.getElementById("calculator");
 const tooBichih = (number) => {
   niittoo.innerHTML += number;
+  if (niittoo.innerHTML.length > 16) {
+    niittoo.style.fontSize = "24px";
+  } else if (niittoo.innerHTML.length > 12) {
+    niittoo.style.fontSize = "32px";
+  } else if (niittoo.innerHTML.length > 10) {
+    niittoo.style.fontSize = "40px";
+  }
 };
 const AC = () => {
   niittoo.innerHTML = "";
+  niittoo.style.fontSize = "48px";
 };
 const tseg = () => {
   if (niittoo.innerHTML[niittoo.innerHTML.length - 1] !== ".") {
     niittoo.innerHTML += ".";
   }
+  if (niittoo.innerHTML.length > 16) {
+    niittoo.style.fontSize = "24px";
+  } else if (niittoo.innerHTML.length > 12) {
+    niittoo.style.fontSize = "32px";
+  } else if (niittoo.innerHTML.length > 10) {
+    niittoo.style.fontSize = "40px";
+  }
 };
 let hadaglah;
-var hi;
+let hi;
 const vildel = (operator) => {
   hi = operator;
   hadaglah = niittoo.innerHTML;
   AC();
+  if (niittoo.innerHTML.length > 16) {
+    niittoo.style.fontSize = "24px";
+  } else if (niittoo.innerHTML.length > 12) {
+    niittoo.style.fontSize = "32px";
+  } else if (niittoo.innerHTML.length > 10) {
+    niittoo.style.fontSize = "40px";
+  }
 };
 const nemehHasah = () => {
   niittoo.innerHTML = number(niittoo.innerHTML) * "-1";
+  if (niittoo.innerHTML.length > 16) {
+    niittoo.style.fontSize = "24px";
+  } else if (niittoo.innerHTML.length > 12) {
+    niittoo.style.fontSize = "32px";
+  } else if (niittoo.innerHTML.length > 10) {
+    niittoo.style.fontSize = "40px";
+  }
 };
 const tentsvv = () => {
   if (hi === "-") {
@@ -32,4 +61,66 @@ const tentsvv = () => {
   } else if (hi === "%") {
     niittoo.innerHTML = (Number(hadaglah) / 100) * Number(niittoo.innerHTML);
   }
+  if (niittoo.innerHTML.length > 16) {
+    niittoo.style.fontSize = "24px";
+  } else if (niittoo.innerHTML.length > 12) {
+    niittoo.style.fontSize = "32px";
+  } else if (niittoo.innerHTML.length > 10) {
+    niittoo.style.fontSize = "40px";
+  }
 };
+document.addEventListener("keydown", (event) => {
+  if (event.key === "c") AC();
+});
+document.addEventListener("keydown", (event) => {
+  if (
+    (event.key.charCodeAt() > 47 && event.key.charCodeAt() < 58) ||
+    event.key.charCodeAt() === 46
+  ) {
+    if (event.key.charCodeAt() === 46) {
+      tseg();
+    } else {
+      niittoo.innerHTML += event.key;
+    }
+  }
+});
+document.addEventListener("keydown", (event) => {
+  if (event.key === "-" ) vildel();
+});
+document.addEventListener("keydown", (event) => {
+  if (event.key === "+") vildel();
+});
+document.addEventListener("keydown", (event) => {
+  if (event.key === "*" ) vildel();
+});
+document.addEventListener("keydown", (event) => {
+  if (event.key ===  "/" ) vildel();
+});
+document.addEventListener("keydown", (event) => {
+  if (event.key === "%") vildel();
+});
+document.addEventListener("keydown", (event) => {
+  if (event.key === "v") niittoo.innerHTML = number(niittoo.innerHTML) * "-1";
+});
+document.addEventListener("keydown", (event) => {
+  if (event.key === "="){
+    if (hi === "-") {
+      niittoo.innerHTML = Number(niittoo.innerHTML) - Number(hadaglah);
+    } else if (hi === "+") {
+      niittoo.innerHTML = Number(niittoo.innerHTML) + Number(hadaglah);
+    } else if (hi === "*") {
+      niittoo.innerHTML = Number(niittoo.innerHTML) * Number(hadaglah);
+    } else if (hi === "/") {
+      niittoo.innerHTML = Number(hadaglah) / Number(niittoo.innerHTML);
+    } else if (hi === "%") {
+      niittoo.innerHTML = (Number(hadaglah) / 100) * Number(niittoo.innerHTML);
+    }
+    if (niittoo.innerHTML.length > 16) {
+      niittoo.style.fontSize = "24px";
+    } else if (niittoo.innerHTML.length > 12) {
+      niittoo.style.fontSize = "32px";
+    } else if (niittoo.innerHTML.length > 10) {
+      niittoo.style.fontSize = "40px";
+    }
+  };
+});
