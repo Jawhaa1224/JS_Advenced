ce8b602d36eee6b8254b6732818489b274941c9f
Jawhaa1224 <jawkhlan626@gmail.com>
2022-09-16 20:09:30 +0800

    haha

--- JS_6/stop_watch.js
+++ JS_6/stop_watch.js
@@ -1,11 +1,23 @@
 const Start = document.getElementById("start");
-const Stop = document.getElementById("stop");
 const Pause = document.getElementById("pause");
-const Minuts = document.getElementById("Minuts");
-const Seconds = document.getElementById("Seconds");
-const Tens = document.getElementById("Tens");
+const Reset = document.getElementById("reset");
+const Minuts = document.getElementById("minuts");
+const Seconds = document.getElementById("seconds");
+const Tens = document.getElementById("tens");
 const start = () => {
-  Tens.setMilliseconds();
+  const zogsoh = setInterval(() => {
+    Tens.innerHTML = Number(Tens.innerHTML) + 1;
+    if (Tens.innerHTML == 100) {
+      Seconds.innerHTML = Number(Seconds.innerHTML) + 1;
+      if (Seconds.innerHTML == 60) {
+        Seconds.innerHTML = 0;
+        Minuts.innerHTML = Minuts.innerHTML + 1;
+      }
+      Tens.innerHTML = 00;
+    }
+  }, 10);
 };
-const stop = () => {};
-const pause = () => {};
+const pause = () => {
+  clearInterval(zogsoh);
+};
+const reset = () => {};
--- JS_8/fetch.js
+++
@@ -1,8 +0,0 @@
-const myAsync = async () => {
-  const response = await fetch("https://www.randomuser.me/api/?results=50");
-  //   let data = await response.json();
-  console.log(response);
-};
-const myFunction = () => {
-  myAsync();
-};
